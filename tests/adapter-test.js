d811b400ff0d2b9008f09e696ffa9156602bc181
Kjell-Morten <post@kjellmorten.no>
2018-03-01 13:57:31 +0100

    Upgrade deps and fix lints

--- tests/adapter-test.js
+++ tests/adapter-test.js
@@ -39,7 +39,7 @@ const defs = {sources, datatypes, mappings}
 test('should retrieve from couchdb', async (t) => {
   nock('http://test.api')
     .get('/article1')
-      .reply(200, {_id: 'article1', type: 'article'})
+    .reply(200, {_id: 'article1', type: 'article'})
   const resources = couchdb(integreat.resources())
   const great = integreat(defs, resources)
   const action = {type: 'GET', payload: {id: 'article1', type: 'article'}}
@@ -53,7 +53,7 @@ test('should retrieve from couchdb', async (t) => {
 test('should send to couchdb', async (t) => {
   const scope = nock('http://test.api')
     .put('/article2', (body) => body._id === 'article2')
-      .reply(201, {_id: 'article2', _rev: '1-8371734'})
+    .reply(201, {_id: 'article2', _rev: '1-8371734'})
   const resources = couchdb(integreat.resources())
   const great = integreat(defs, resources)
   const action = {type: 'SET', payload: {data: {id: 'article2', type: 'article'}}}
@@ -68,9 +68,9 @@ test('should send to couchdb', async (t) => {
 test('should get rev before sending to couchdb', async (t) => {
   const scope = nock('http://test.api')
     .post('/_all_docs', {keys: ['article3']})
-      .reply(200, {rows: [{id: 'article3', key: 'article3', value: {rev: '1-8371734'}}]})
+    .reply(200, {rows: [{id: 'article3', key: 'article3', value: {rev: '1-8371734'}}]})
     .put('/article3', (body) => body._rev === '1-8371734')
-      .reply(201, {_id: 'article3', _rev: '2-3139483'})
+    .reply(201, {_id: 'article3', _rev: '2-3139483'})
   const resources = couchdb(integreat.resources())
   const great = integreat(defs, resources)
   const action = {type: 'SET', payload: {data: {id: 'article3', type: 'article'}}}
