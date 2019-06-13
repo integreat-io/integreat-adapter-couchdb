f4e801229b117ba463213a3853b9d18a1734d741
Kjell-Morten <post@kjellmorten.no>
2019-06-13 21:29:34 +0200

    Fix lint errors

--- tests/adapter-test.js
+++ tests/adapter-test.js
@@ -11,7 +11,7 @@ const sources = [{
   adapter: 'couchdb',
   baseUri: 'http://test.api',
   endpoints: [
-    {scope: 'member', options: {uri: '/{id}'}}
+    { scope: 'member', options: { uri: '/{id}' } }
   ]
 }]
 
@@ -32,17 +32,17 @@ test.after.always((t) => {
   nock.restore()
 })
 
-const defs = {sources, datatypes, mappings}
+const defs = { sources, datatypes, mappings }
 
 // Tests
 
 test('should retrieve from couchdb', async (t) => {
   nock('http://test.api')
     .get('/article1')
-    .reply(200, {_id: 'article1', type: 'article'})
+    .reply(200, { _id: 'article1', type: 'article' })
   const resources = couchdb(integreat.resources())
   const great = integreat(defs, resources)
-  const action = {type: 'GET', payload: {id: 'article1', type: 'article'}}
+  const action = { type: 'GET', payload: { id: 'article1', type: 'article' } }
 
   const ret = await great.dispatch(action)
 
@@ -53,10 +53,10 @@ test('should retrieve from couchdb', async (t) => {
 test('should send to couchdb', async (t) => {
   const scope = nock('http://test.api')
     .put('/article2', (body) => body._id === 'article2')
-    .reply(201, {_id: 'article2', _rev: '1-8371734'})
+    .reply(201, { _id: 'article2', _rev: '1-8371734' })
   const resources = couchdb(integreat.resources())
   const great = integreat(defs, resources)
-  const action = {type: 'SET', payload: {data: {id: 'article2', type: 'article'}}}
+  const action = { type: 'SET', payload: { data: { id: 'article2', type: 'article' } } }
 
   const ret = await great.dispatch(action)
 
@@ -67,13 +67,13 @@ test('should send to couchdb', async (t) => {
 
 test('should get rev before sending to couchdb', async (t) => {
   const scope = nock('http://test.api')
-    .post('/_all_docs', {keys: ['article3']})
-    .reply(200, {rows: [{id: 'article3', key: 'article3', value: {rev: '1-8371734'}}]})
+    .post('/_all_docs', { keys: ['article3'] })
+    .reply(200, { rows: [{ id: 'article3', key: 'article3', value: { rev: '1-8371734' } }] })
     .put('/article3', (body) => body._rev === '1-8371734')
-    .reply(201, {_id: 'article3', _rev: '2-3139483'})
+    .reply(201, { _id: 'article3', _rev: '2-3139483' })
   const resources = couchdb(integreat.resources())
   const great = integreat(defs, resources)
-  const action = {type: 'SET', payload: {data: {id: 'article3', type: 'article'}}}
+  const action = { type: 'SET', payload: { data: { id: 'article3', type: 'article' } } }
 
   const ret = await great.dispatch(action)
 
